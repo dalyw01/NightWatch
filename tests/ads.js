67120df2a255c1bdc9d38ba2f8019953d2335c93
William Daly <dalyw01@MC-S104581.lan>
2020-03-13 16:32:20 +0000

    finishing with ads

--- tests/ads.js
+++ tests/ads.js
@@ -4,35 +4,22 @@ module.exports = {
 
   before : function (browser) // Before the whole test is run
   {
-      browser.resizeWindow(1500, 1500);
+      browser.resizeWindow(1500, 1500)
+      .url('https://is.gd/afitip')
+      .click('div#content input#adType-skippable')
+      .click('button#loadAdButton')
+      .pause(1000)
   },
 
   'Skippable Ads': function (browser) 
   {
-    browser.url('https://is.gd/afitip')
-    .click('div#content input#adType-skippable')
-    .click('button#loadAdButton')
-    .pause(1000)
-    .frame('smphtml5iframemp')
-      .click('div.p_accessibleHitArea')
-      .click('div.p_accessibleHitArea')
-      .pause(3000)
-      //.click('.videoAdUiSkipIcon')
-      //.frame(null)
-      .frame('ima-sdk-frame')
-        .click('body > div.videoAdUi.imaVideoAdUi > div.videoAdUiBottomBar > div > div.videoAdUiSkipContainer.html5-stop-propagation > button')
-    //.assert.cssProperty('h1', 'font-family' , 'ReithSans, Arial, Helvetica, freesans, sans-serif')
-    // we are now back to the main page
-    // ... more code here ...
-    //.click('.videoAdUi')
-      //.click('.videoAdUiSkipButton')
-        //videoAdUiSkipButton videoAdUiAction videoAdUiRedesignedSkipButton
-        //.click('.videoAdUiRedesignedSkipButton')
-      //.pause(10000)
-      // .click('div.p_accessibleHitArea')
-      // .click('div.p_accessibleHitArea')
-      // .click('.p_pauseIcon')
-      //.pause(5000)
+    browser
+      .frame('smphtml5iframemp')
+        .pause(1000)
+        .click('div.p_accessibleHitArea')
+        .click('div.p_accessibleHitArea')
+        .pause(4000)
+        .click('#ads_adsUX')
     browser.end();
   }
 
