635f7b3e7d178a12fba821fe6c4542ab66f8835d
William Daly <william.daly@bbc.co.uk>
2020-05-07 15:03:49 +0100

    Update multiple_pages_using_forloop_check_aria_label.js

--- tests/multiple_pages_using_forloop_check_aria_label.js
+++ tests/multiple_pages_using_forloop_check_aria_label.js
@@ -10,8 +10,8 @@ module.exports = {
   'Test 1 - Run through core functionality for a media player on different sites': function (browser) {
 
     links = [ 
-      'https://is.gd/averud',
-      'https://is.gd/averud'
+      'https://www.website1.com',
+      'https://www.website2.com'
     ];
 
     for (i = 0; i < links.length; i++) 
